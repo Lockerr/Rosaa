db2717116a023f5e03845cfccb3e0919b4886849
renius <antiqe@gmail.com>
2012-10-24 10:27:27 +0600

    add role to users

--- db/migrate/20121024033940_add_role_to_user.rb
+++ db/migrate/20121024033940_add_role_to_user.rb
@@ -1,4 +1,5 @@
 class AddRoleToUser < ActiveRecord::Migration
   def change
+  	add_column :users, :role, :tinyint, :default => 0
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111116042051) do
+ActiveRecord::Schema.define(:version => 20121024033940) do
 
   create_table "comments", :force => true do |t|
     t.string   "title",            :limit => 50, :default => ""
@@ -38,19 +38,36 @@ ActiveRecord::Schema.define(:version => 20111116042051) do
     t.integer  "comments_count"
   end
 
+  create_table "regions", :force => true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "taxes", :force => true do |t|
+    t.integer  "region_id"
+    t.float    "rate"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer  "vehicle_id"
+    t.string   "title"
+    t.integer  "min"
+    t.integer  "max"
+  end
+
   create_table "users", :force => true do |t|
-    t.string   "email",                  :default => "", :null => false
-    t.string   "encrypted_password",     :default => "", :null => false
+    t.string   "email",                               :default => "", :null => false
+    t.string   "encrypted_password",                  :default => "", :null => false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          :default => 0
+    t.integer  "sign_in_count",                       :default => 0
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.datetime "created_at",                             :null => false
-    t.datetime "updated_at",                             :null => false
+    t.datetime "created_at",                                          :null => false
+    t.datetime "updated_at",                                          :null => false
     t.string   "first_name"
     t.string   "phone"
     t.string   "second_name"
@@ -59,6 +76,7 @@ ActiveRecord::Schema.define(:version => 20111116042051) do
     t.string   "confirmation_token"
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
+    t.integer  "role",                   :limit => 1, :default => 0
   end
 
   add_index "users", ["confirmation_token"], :name => "index_users_on_confirmation_token", :unique => true
