07bd9c8ef501bdc84c7ccd3f32971008e8441534
renius <antiqe@gmail.com>
2012-10-24 10:13:58 +0600

    solved #1

--- app/views/ideas/devote.js
+++ app/views/ideas/devote.js
@@ -1,2 +1,5 @@
-$('div#idea_<%= @idea.id %>.b-post-karma__amount').html(<%= @idea.likes.size - @idea.dislikes.size %>);
+idea = $('div#idea_<%= @idea.id %>')
+idea.find('.b-post-karma__amount').html('<%= @idea.likes.size - @idea.dislikes.size %>');
+idea.find('.vote, .devote').css('background-color', '#c9c9c9')
+idea.find('.b-post-karma__votes').prepend('<span>Ваш голос принят!</span><br/>')
 
---
+++ db/migrate/20121024033940_add_role_to_user.rb
@@ -0,0 +1,4 @@
+class AddRoleToUser < ActiveRecord::Migration
+  def change
+  end
+end
